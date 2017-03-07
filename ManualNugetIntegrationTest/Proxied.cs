e0b34298355c5b47bd5de292d9e3fdec18f539b1
Bruno Juchli <bruno.juchli@innocreative.ch>
2017-03-07 17:23:42 +0700

    updated integration test

--- ManualNugetIntegrationTest/Proxied.cs
+++ ManualNugetIntegrationTest/Proxied.cs
@@ -5,9 +5,12 @@
     [StaticProxy]
     public class Proxied
     {
+        private static int RandomValue = (new Random()).Next(-100, 100);
         public void Foo()
         {
-            Console.WriteLine("inside original implementation of Proxied.Foo()");
+            Console.WriteLine(
+                "inside original implementation of Proxied.Foo() with static random value {0}", 
+                RandomValue);
         }
     }
 }
